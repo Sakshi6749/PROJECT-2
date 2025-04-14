8d1b4be8318054217a48c8fdef78b6a71035e1eb
Sakshi6749 <24f2006749@ds.study.iitm.ac.in>
2025-04-14 16:19:40 +0530

    Update Q26.py

--- GA2/Q26.py
+++ GA2/Q26.py
@@ -1,4 +1,4 @@
 import os, json
 
 def execute(question: str, parameter):
-    return "https://hub.docker.com/repository/docker/saaakshi2docker/py-hello/general"
+    return "https://for-project2.vercel.app/api"
