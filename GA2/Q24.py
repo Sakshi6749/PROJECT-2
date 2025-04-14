7b0c10b90a198a9f544a425674ca1c744be8abf9
Sakshi6749 <24f2006749@ds.study.iitm.ac.in>
2025-04-14 15:55:39 +0530

    Update Q24.py

--- GA2/Q24.py
+++ GA2/Q24.py
@@ -4,7 +4,8 @@ def execute(question: str, parameter):
     results = {
         "22f2000946@ds.study.iitm.ac.in":"b6946", # Akash
         "22f3002560@ds.study.iitm.ac.in": "23a99", # Prabhnoor
-        "23f2004837@ds.study.iitm.ac.in": "46936"
+        "23f2004837@ds.study.iitm.ac.in": "46936",
+        "24f2006749@ds.study.iitm.ac.in": "c4422"
     }
     if parameter["email"] not in results:
         return {"error": "No use case found."}
