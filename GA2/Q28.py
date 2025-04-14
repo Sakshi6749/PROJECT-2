a7b1a739fc1d5e34c7fafeb42364367c8a6109f4
Sakshi6749 <24f2006749@ds.study.iitm.ac.in>
2025-04-14 16:20:10 +0530

    Update Q28.py

--- GA2/Q28.py
+++ GA2/Q28.py
@@ -1,4 +1,4 @@
 import os, json
 
 def execute(question: str, parameter):
-    return "https://hub.docker.com/repository/docker/vinilmtii/nodeapi/general"
+    return "https://hub.docker.com/repository/docker/saaakshi2docker/py-hello/general"
